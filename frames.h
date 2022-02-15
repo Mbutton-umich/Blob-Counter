0afa03a96344fd8944c2887a936bd79d5e55ce63
Mbutton-umich <73185309+Mbutton-umich@users.noreply.github.com>
2022-02-14 20:12:40 -0500

    Version: 1.2  RBT Update

--- frames.h
+++ frames.h
@@ -1,6 +1,6 @@
 /*EECS 300 Final Project Code Team 11: frames.h
-Version: 1.0  Beta
-Updated: FRI11FEB22
+Version: 1.2  RBT Update
+Updated: MON14FEB22
 
 Frames tracks blobs between caputred therma camera frames
 All functions have been tested on 9x9 Arrays
@@ -9,6 +9,7 @@ TODO: is a location I need to come back for some reason
 OPTIM: is a location that is marked for potential improvement
 !!!!: is a location with specific and dangerous importance
 */
+#pragma once
 
 #ifndef FRAMES_H
 #define FRAMES_H
@@ -17,6 +18,8 @@ OPTIM: is a location that is marked for potential improvement
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
+
+#include "rBT.h"
 
 
 #define NEIGHNUM 8
@@ -52,14 +55,12 @@ float distCalc(float r1_in, float c1_in, float r2_in, float c2_in);
 float vertDistCalc(float r1_in, float r2_in);
 //Comparator used by qsort() to arrange the distance matrix in ascendign order
 int distComp(const void* a, const void* b);
-//Comparator for qsort() to arrange blockList
-int shortComp(const void* a, const void* b);
 //Takes a new and old Blob Location table and computes the distances between all entries
 void fillDist(float oldTable_in[][COORDDIM], short& oldNum_in, float newTable_in[][COORDDIM], short& newNum_in, struct blob dist_in[(BLOBLIM * BLOBLIM)], short& count_in);
 //Matches the new blobs with their closest old blob
-void matchShortest(float oldTable_in[][COORDDIM], short& oldNum_in, float newTable_in[][COORDDIM], short& newNum_in, struct blob dist_in[(BLOBLIM * BLOBLIM)], short& count_in, short blockList_in[BLOBLIM], short& taken_in);
+void matchShortest(float oldTable_in[][COORDDIM], short& oldNum_in, float newTable_in[][COORDDIM], short& newNum_in, struct blob dist_in[(BLOBLIM * BLOBLIM)], short& count_in);
 //Takes care of cases when the new Blob Table is smaller or larger than the old, determines enters/exits
-void orphanCare(float oldTable_in[][COORDDIM], short& oldNum_in, float newTable_in[][COORDDIM], short& newNum_in, short blockList_in[BLOBLIM], short& taken_in, short& crossCount_in);
+void orphanCare(float oldTable_in[][COORDDIM], short& oldNum_in, float newTable_in[][COORDDIM], short& newNum_in, short& crossCount_in);
 //MAnages the full process of updating the old Blob Table to the new Blob Table and counts enters/exits happening during this update
 short updateLocs(float oldTable_in[][COORDDIM], short& oldNum_in, float newTable_in[][COORDDIM], short& newNum_in, struct blob dist_in[(BLOBLIM * BLOBLIM)], short& count_in, short& crossCount_in);
 
