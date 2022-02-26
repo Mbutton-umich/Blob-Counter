5e4a729ed6abd3147f28ef1bf73e9e72d874c2b6
Mbutton-umich <73185309+Mbutton-umich@users.noreply.github.com>
2022-02-26 04:34:20 -0500

    Version: 3.0 Done Deal

--- testUtil.h
+++ testUtil.h
@@ -1,7 +1,6 @@
 /*EECS 300 Final Project Code Team 11: testUtil.h
-Version: 1.6 Cleaned for Speed Test
-Updated: FRI19FEB22
-
+Version: 3.0 Done Deal
+Updated: FRI26FEB22
 
 testUtil functions as a test bench for the frames code. Checking inputs, outputs, and performance time. Also has installation calculation functions.
 
@@ -22,9 +21,9 @@ OPTIM: is a location that is marked for potential improvement
 #include <time.h> //For timing performance
 
 //Reads in test matrices from a folder named "testMats" inside program directory, where files are named test#.txt, where testNum is #
-void readTestMat(int myNums_in[][COL], int testNum_in);
+void readTestMat(bool myNums_in[][COL], int testNum_in);
 //Prints a test matrix for verifiction
-void printTestMat(int myNums_in[][COL]);
+void printTestMat(bool myNums_in[][COL]);
 //Swaps new blob table into old, useful for testing single frame
 void blobSwap();
 //Prints the Blob Table for a Single Frame
@@ -32,13 +31,13 @@ void printBlobTable(const struct blobElem blobTable_in[BLOBLIM], const short blo
 //Prints the Distance Table for old and new frames
 void printDistTable();
 //Prints the tally of crossings
-void printCrossCount();
+void printDeltaPeeps();
 //Prints the number of people inside the room
 void printNumPeep(const short& numPeeps_in);
 //Walkthrough of the two frame update process
-void processWalkthrough(int temp_in[][COL], int oldTestNum_in, int newTestNum_in);
+void processWalkthrough(bool temp_in[][COL], int oldTestNum_in, int newTestNum_in);
 //Does up to a 10 test frame simulation, prints out roomoccupancy after each frame
-void frameSeqTest(int seqLen_in, int startNum_in, int temp_in[][COL], short& peepNum_in);
+void frameSeqTest(int seqLen_in, int startNum_in, bool temp_in[][COL], short& peepNum_in);
 //Takes in a sequence of test matrices and puts them into C form to put into headerfile to loaod onto the ESP32
 void printTestMats2HeaderForm(int seqLen_in, int startNum_in);
 //Gives you install dimensions for a given door width
